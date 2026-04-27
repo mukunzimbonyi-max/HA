8794825352925e6c5e220f61f1b90a3dac71c2fe
Your Name <you@example.com>
2026-04-27 15:56:00 +0200

    Increase body limit to 50mb for image uploads

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Basic Route
